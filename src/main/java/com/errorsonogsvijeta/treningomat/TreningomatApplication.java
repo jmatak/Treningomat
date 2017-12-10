07b7d9faeeb16fa7713c0a7f8fb1bc03a22889f3
Josip Matak <josip.matak@fer.hr>
2017-12-10 18:35:26 +0100

    Inicijalizacija aplikacije

---
+++ src/main/java/com/errorsonogsvijeta/treningomat/TreningomatApplication.java
@@ -0,0 +1,11 @@
+package com.errorsonogsvijeta.treningomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreningomatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TreningomatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/errorsonogsvijeta/treningomat/TreningomatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.errorsonogsvijeta.treningomat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreningomatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
