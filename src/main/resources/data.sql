f9efeb86b6a275f753d032a3a294d0235d1cefa3
Josip Matak <josip.matak@fer.hr>
2017-12-10 21:31:06 +0100

    Inicijalni administrator aplikacije i dopuštene ovlasti.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO administrator VALUES
+  (1, '$2a$04$eALQTd29tHawhmLI23eUVeqnHfRcbL8ulpb4R6dHTiMfZ85bPugPS', 'admin');
+INSERT INTO role VALUES
+  (1, 'ADMIN'), (2, 'TRAINER'), (3, 'ATTENDANT');
+INSERT INTO user_roles VALUES
+  (1, 1);
