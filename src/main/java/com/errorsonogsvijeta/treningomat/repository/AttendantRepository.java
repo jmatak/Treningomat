559b07e89eed46d2b2b7c92c3ba17c826f6d5d34
Patrik Maric <patrik.maric@fer.hr>
2017-12-22 14:11:23 +0100

    Mali ispravak radi rjesavanja konflikta

--- src/main/java/com/errorsonogsvijeta/treningomat/repository/AttendantRepository.java
+++ src/main/java/com/errorsonogsvijeta/treningomat/repository/AttendantRepository.java
@@ -3,15 +3,17 @@ package com.errorsonogsvijeta.treningomat.repository;
 import com.errorsonogsvijeta.treningomat.model.training.TrainingGroup;
 import com.errorsonogsvijeta.treningomat.model.users.Attendant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
- * @author Patrik
+ * TODO: opis
+ *
+ * @author Matej Pipalović
+ * @version 1.0
  */
-@Repository
 public interface AttendantRepository extends JpaRepository<Attendant, Integer> {
+    public Attendant findAttendantByUsername(String username);
 
     List<Attendant> findAllByTrainingGroups(TrainingGroup trainingGroup);
 
