77fa9810bbe74deecae0d87f4fc18a0e88dd140b
Josip Matak <josip.matak@fer.hr>
2017-12-26 20:24:33 +0100

    Dodavanje termina u kalendar

---
+++ src/main/java/com/errorsonogsvijeta/treningomat/model/calendar/Term.java
@@ -0,0 +1,79 @@
+package com.errorsonogsvijeta.treningomat.model.calendar;
+
+import com.errorsonogsvijeta.treningomat.model.training.Training;
+import com.errorsonogsvijeta.treningomat.model.training.TrainingGroup;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Function;
+
+public class Term {
+    private final static String DEFAULT_URL = "/training/";
+    private final static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+    private final static Function<String, String> formatter = s -> s.substring(0, 10) + "T" + s.substring(11, s.length());
+
+    private String title;
+    private String url;
+    private String start;
+    private String end;
+
+    public Term() {
+    }
+
+    public Term(TrainingGroup trainingGroup, Integer id, Date startAt, Date endsAt) {
+        title = trainingGroup.getName();
+        url = DEFAULT_URL + id;
+        start = formatter.apply(DATE_FORMAT.format(startAt));
+        end = formatter.apply(DATE_FORMAT.format(endsAt));
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public static List<Term> toTerms(List<Training> trainings) {
+        List<Term> terms = new ArrayList<>();
+        for (Training training : trainings) {
+            terms.add(Term.toTerm(training));
+        }
+        return terms;
+    }
+
+    private static Term toTerm(Training training) {
+        return new Term(training.getTrainingGroup(),
+                training.getId(),
+                training.getStartAt(),
+                training.getEndsAt());
+    }
+}
---
+++ src/main/java/com/errorsonogsvijeta/treningomat/repository/TrainingRepository.java
@@ -0,0 +1,8 @@
+package com.errorsonogsvijeta.treningomat.repository;
+
+import com.errorsonogsvijeta.treningomat.model.training.Training;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainingRepository extends JpaRepository<Training, Integer> {
+
+}
