71b31ea500ec5540eaa3ac563ba58d9909dc28b4
Josip Matak <josip.matak@fer.hr>
2017-12-26 20:24:33 +0100

    Dodavanje termina u kalendar

---
+++ src/main/java/com/errorsonogsvijeta/treningomat/controllers/TrainingController.java
@@ -0,0 +1,58 @@
+package com.errorsonogsvijeta.treningomat.controllers;
+
+import com.errorsonogsvijeta.treningomat.model.training.Training;
+import com.errorsonogsvijeta.treningomat.model.users.Trainer;
+import com.errorsonogsvijeta.treningomat.services.TrainerService;
+import com.errorsonogsvijeta.treningomat.services.TrainingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/training")
+public class TrainingController {
+    @Autowired
+    private TrainerService trainerService;
+
+    @Autowired
+    private TrainingService trainingService;
+
+    @RequestMapping(value = "/newTraining", method = RequestMethod.GET)
+    public ModelAndView newTraining() {
+        ModelAndView modelAndView = new ModelAndView("calendar/new_training");
+        Trainer trainer = getLoggedUser();
+        modelAndView.addObject("training", new Training());
+        modelAndView.addObject("allGroups", trainer.getTrainingGroups());
+        return modelAndView;
+    }
+    @RequestMapping(value = "/newTraining", method = RequestMethod.POST)
+    public ModelAndView newTraining(Training training, BindingResult result) {
+        trainingService.save(training);
+        ModelAndView modelAndView = new ModelAndView("redirect:training/new_training");
+        return modelAndView;
+    }
+
+    private Trainer getLoggedUser() {
+        org.springframework.security.core.userdetails.User loggedUser = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return trainerService.findTrainerByUsername(loggedUser.getUsername());
+    }
+
+    @InitBinder
+    public void bindingPreparation(WebDataBinder binder) {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy/HH:mm");
+        CustomDateEditor orderDateEditor = new CustomDateEditor(dateFormat, true);
+        binder.registerCustomEditor(Date.class, orderDateEditor);
+    }
+}
